e5d5154f581bd454a5100d391ea3851755bd1bb1
emreipekci <153276209+emreipekci@users.noreply.github.com>
2024-12-13 20:18:17 +0100

    set up project structure

--- project.js
+++ project.js
@@ -4,17 +4,17 @@ const myLibrary = [];
 function Book(title, author, pages, yearPublished, isRead) {    
   this.title = title;
   this.author = author;
-  this.pages = pages;
   this.yearPublished = yearPublished;
+  this.pages = pages;
   this.isRead = isRead;
 
   this.getInfo = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.yearPublished}. Read: ${this.isRead ? "yes" : "no"}`;
+    return `${this.title} by ${this.author}, ${this.yearPublished}, ${this.pages} pages. Read: ${this.isRead ? "yes" : "no"}`;
   }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary() {     // do stuff here.. when i click the button, it opens a form card with submit.
+ 
 }
 
 theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
